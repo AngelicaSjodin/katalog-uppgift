a4fb7988b6494a9f87d2a7186c9c99d0afce4fa2
AdrianK2001 <3tomu24.kaczad@folkuniversitetet.nu>
2024-11-07 11:22:21 +0100

    Catalog

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Catalog {
+
+    private List<Products> products;
+
+    public Catalog() {
+        products = new ArrayList<>();
     }
+
+    //Lägga till en produkt
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    //Visa alla produkter
+    public void showAllProducts() {
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+    //Testar Catalog klass
+    public static void main(String[] args) {
+        Catalog catalog = new Catalog();
+
+        //Skapar och lägger till två produkter
+        Product product1 = new Product("Laptop", 999.99);
+        Product product2 = new Product("Iphone", 699.99);
+
+        catalog.addProduct(product1);
+        catalog.addProduct(product2);
+
+        //Visa alla produkter
+        catalog.showAllProducts();
+    }
+
 }
