0a11c25ad20d762303c4934b87c5f4eb95b9ffa8
umutuca <3tomu24.ucarum@folkuniversitetet.nu>
2024-11-07 11:24:33 +0100

    Del 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Katalog katalog = new Katalog();
+        Scanner scanner = new Scanner (System.in);
+
+    while (true) {
+        System.out.println("Meny");
+        System.out.println("1 Lägg till produkt");
+        System.out.println("2 Visa katalogen");
+        System.out.println("3 Stäng");
+
+    int choice = scanner.nextInt();
+    scanner.nextLine();
+
+    if (choice == 1) {
+
+
+
+    else if (choice == 2) {
+
+
+
+    else if (choice == 3) {
+
+
+   else {
+                    System.out.println("Försök igen");
+                }
+
     }
 }
